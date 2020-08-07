29c245e6166a9e222302dd931ad0aafa58c6f999
alexanderajju <alexanderajju@gmail.com>
2020-08-07 12:39:27 +0530

    messages

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ io.on("connection", (socket) => {
     socket.join(roomId);
     console.log("joined room");
     socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message);
+    });
   });
 });
 
